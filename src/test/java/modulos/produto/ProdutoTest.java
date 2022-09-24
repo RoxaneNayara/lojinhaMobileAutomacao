c702c236ab953d92649b02635ece2e1471791bbf
Roxane <roxane.nayara@gmail.com>
2022-09-23 21:45:36 -0300

    Inicializando o repositório com os arquivos iniciais do projeto

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,60 @@
+package modulos.produto;
+
+import io.appium.java_client.AppiumDriver;
+import modulos.usuario.tela.LoginTela;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+@DisplayName("Teste Mobile do Módulo de Produto")
+public class ProdutoTest {
+
+    private WebDriver app;
+
+    @BeforeEach
+    public void beforeEach() throws MalformedURLException {
+        //Abrir o App
+        DesiredCapabilities capacidades = new DesiredCapabilities();
+        capacidades.setCapability("deviceName", "ASUS_X00QD");
+        capacidades.setCapability("platform", "Android");
+        capacidades.setCapability("udid", "JBAXB603Z10693L");
+        capacidades.setCapability("appPackage", "com.lojinha");
+        capacidades.setCapability("appActivity", "com.lojinha.ui.MainActivity");
+        capacidades.setCapability("app", "C:\\Lojinha App\\Lojinha+Android+Nativa\\lojinha-nativa.apk");
+
+        this.app = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capacidades);
+        this.app.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @DisplayName("Validacao do Valor do Produto Nao Permitido")
+    @Test
+    public void testValidacaoDoValorDeProdutoNaoPermitido() {
+        //Fazer Login
+        String mensagemApresentada = new LoginTela(app)
+                .preencherUsuario("admin")
+                .preencherSenha("admin")
+                .submeterLogin()
+                .abrirTelaAdicaoProduto()
+                .preencherNomeProduto("Iphone")
+                .preencherValorProduto("700001")
+                .preencherCoresProduto("preto,prata")
+                .submissaoComErro()
+                .obterMensagemDeErro();
+
+        //Validar que a mensagem de valor inválido foi apresentada
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+    }
+        @AfterEach
+        public void afterEach() { app.quit();
+
+    }
+}
+
+
---
+++ src/test/java/modulos/usuario/tela/BaseTela.java
@@ -0,0 +1,15 @@
+package modulos.usuario.tela;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTela {
+    protected WebDriver app;
+
+    public BaseTela(WebDriver app){
+        this.app = app;
+    }
+    public String capturarToast (){
+        return app.findElement(By.xpath("//android.widget.Toast")).getText();
+    }
+}
